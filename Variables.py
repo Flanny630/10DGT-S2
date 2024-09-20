7d1ff4bdf03a3574b4784dcafadbd3ea25a2af88
Flanny630 <flanny.xue@kingsway.school.nz>
2024-09-20 14:50:36 +1200

    Variables

---
+++ Variables.py
@@ -0,0 +1,29 @@
+# Showing how variables work
+# Author: Flanny Xue
+# Date: 20-09-2024
+# Version 1
+
+
+# Different types of variables
+# Store a number
+num_1 = 80 # assigning 80 to num_1
+print(num_1) # This displays what num_1 contains
+
+# Store a string
+name= "Flanny"
+print(name)
+name= "Hey~" # reassinging value to an existing variable
+print(name)
+name_1= "XUe"
+print (name_1)
+
+# Do calculations in a variable and store the result
+sum=5+5 # not good practice
+sum = 5 + 5 # follows PEP8 Python conventions
+print(sum) 
+sum_string = "5 + 5 = 10" # this is a string
+print(sum)
+
+# Adding strings together is called concatenation
+concatenation = name_1 + sum_string
+print(concatenation)
