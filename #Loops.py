1d1bcb93d6a3fd913c3f046045159d1e010fcbab
Flanny630 <flanny.xue@kingsway.school.nz>
2024-09-25 10:00:14 +1200

    Loops

---
+++ #Loops.py
@@ -0,0 +1,21 @@
+#Loops and indents
+# Author: Flanny Xue
+# Date: 20-09-2024
+# Version 1
+
+
+# for loops, repeat for a set number of times.
+# for starts the loop
+# next is the name of the loop
+# in range tells the loop how many times to run
+# the number in the () is how many times it repeats
+for name_of_loop in range(5):
+    print("ha")
+
+# while loop, runs unti a condition is met or no longer is met
+keep_going = "" # empty variable
+while keep_going == "":
+    print("looping")
+    print("still looping")
+
+    keep_going = input("Again? Or press any other key to quit")
